9390651c2bd6332c7122538543b0e38ce7ab6ec7
Damali Boyd <damaliboyd@ymail.com>
2021-03-15 08:26:43 -0400

    push up

---
+++ Core/Entities/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Author
+    {
+        public string FirstName;
+        public string LastName;
+        public ICollection<Book> Books;
+}
---
+++ Core/Entities/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Review
+    {
+        public string Reviewer { get; set; }
+        public string Text { get; set; }
+        public Book Book { get; set; }
+    }
+}
